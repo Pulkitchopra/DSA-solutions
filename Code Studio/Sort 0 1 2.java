e7a2aea340af1f2b878c56f27ee6cefe756b4f4c
Pulkit Chopra <65125133+Pulkitchopra@users.noreply.github.com>
2023-09-12 22:12:57 +0530

    Create Sort 0 1 2.java

---
+++ Code Studio/Sort 0 1 2.java
@@ -0,0 +1,42 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+    public static void sort012(int[] arr)
+    {
+
+        int low = 0; int mid = 0; int high = arr.length - 1;
+
+        while(mid<=high){
+
+
+
+            if(arr[mid]==0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+
+                arr[mid] = temp;
+
+                low++;
+                mid++;
+
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+
+
+
+            else{
+                
+                int temp = arr[mid];
+
+                arr[mid] = arr[high];
+
+                arr[high] = temp;
+
+                high--;
+            }
+        }
+    }
+}
