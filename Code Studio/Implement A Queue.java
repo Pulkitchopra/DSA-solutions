16c4cd73d966da17a52985dbf5ac486d384d2738
Pulkit Chopra <65125133+Pulkitchopra@users.noreply.github.com>
2023-11-02 22:15:52 +0530

    Create Implement A Queue.java

---
+++ Code Studio/Implement A Queue.java
@@ -0,0 +1,85 @@
+public class Queue {
+
+
+    int arr[];
+
+    int qfront;
+    int rear;
+    int size;
+
+
+
+    Queue() {
+        size = 100001;
+        arr = new int[size];
+
+        qfront = 0;
+
+        rear = 0;  
+    }
+
+
+    boolean isEmpty() {
+       if(qfront == rear){
+
+
+
+
+           return true;
+       }
+       else{
+
+           return false;
+       }
+    }
+
+    void enqueue(int data) {
+        if(rear == size){
+            return;
+        }
+        else{
+
+            arr[rear] = data;
+
+            rear++;     
+        }
+    }
+
+
+
+    int dequeue() {
+        
+        if(qfront == rear){
+
+            return -1;
+        }
+        else{
+
+            int ans = arr[qfront];
+            arr[qfront] = -1;
+
+            qfront++;
+            if(qfront == rear){
+                qfront = 0;
+                rear = 0;
+            }
+            return ans;
+        }
+    }
+
+
+    
+
+    int front() {
+
+
+        if(qfront == rear){
+            return -1;
+        }
+        else{
+
+            return arr[qfront];
+        }
+    }
+
+}
