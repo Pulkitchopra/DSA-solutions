0365bceb8f9e0078468cfc447efa4afd9b456cf8
Pulkit Chopra <65125133+Pulkitchopra@users.noreply.github.com>
2024-01-03 18:48:49 +0530

    Update Maximum Subarray Sum.java

--- Code Studio/Maximum Subarray Sum.java
+++ Code Studio/Maximum Subarray Sum.java
@@ -1,20 +1,17 @@
-import java.util.* ;
-import java.io.*; 
-
 public class Solution {
 	
 	public static long maxSubarraySum(int[] arr, int n) {
-		
-
 		long sum = 0;
 		long maxi = arr[0];
 		for(int i = 0; i < n; i++){
-			sum = sum + nums[i];
+			sum = sum + arr[i];
 			maxi = Math.max(maxi, sum);
 			if(sum < 0){
 				sum = 0;
 			}
 		}
+		if(maxi<0)
+		return 0;
 		return maxi;
 	}
 
